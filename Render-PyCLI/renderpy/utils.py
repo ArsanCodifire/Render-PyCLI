74911e548026c32f72103caec197fcfa89edd969
Arsan Codifire <arsancodifire@signinid.com>
2025-08-29 00:30:47 +0530

    Update utils.py

--- Render-PyCLI/renderpy/utils.py
+++ Render-PyCLI/renderpy/utils.py
@@ -4,7 +4,7 @@ import click
 RENDER_API_BASE = "https://api.render.com/v1"
 
 def require_api_key():
-    key = os.getenv("RENDER_API_KEY")
+    key = os.environ.get("RENDER_API")
     if not key:
         click.secho("❌ RENDER_API_KEY environment variable not set.", fg="red")
         raise SystemExit(1)
